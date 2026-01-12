499bc419b5c0404f1af48179be1e476f8b5a6abd
Lalit <lalitnandan19@gmail.com>
2026-01-12 15:34:02 +0530

    email reminder

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -68,7 +68,7 @@ const sendConnectionRequestReminder = inngest.createFunction(
     const {connectionId}=event.data;
 
     await step.run('send-connection-request-mail',async()=>{
-      const connection=await Connection.findById(connectionId).populate('from_user_id','to_user_id');
+      const connection=await Connection.findById(connectionId).populate('from_user_id to_user_id');
       const subject=`New Connection Request`;
       const body=`
       <div style="font-family:Arial,sans-serif;padding:20px;>
@@ -87,7 +87,35 @@ const sendConnectionRequestReminder = inngest.createFunction(
       })
 
       // Want to send remainder in 24 hours
-      // ...
+      const in24Hours = new Date((Date.now() + 60*60*1000))
+      await step.sleepUntil("wait-for-24-hours",in24Hours);
+      await step.run('send-connection-request-reminder',async()=>{
+        const connection=await Connection.findById(connectionId).populate('from_user_id to_user_id');
+
+        if(connection.status === "accepted"){
+          return {message: "Already accepted"}
+        }
+
+      const subject=`New Connection Request`;
+      const body=`
+      <div style="font-family:Arial,sans-serif;padding:20px;>
+      <h2>Hi ${connection.to_user_id.full_name},</h2>
+      <p>You have a new connection request from ${connection.from_user_id.full_name}-@${connection.from_user_id.username}</p>
+      <p>Click <a heref="${process.env.FRONTEND_URL}/connections" style="color:#10b981;"> here </a> to accept or reject the request </p>
+      <br/>
+      <p>Thanks, <br/> Incanvas - Stay Connected</p>
+      </div>
+      `;
+
+      await sendEmail({
+        to : connection.to_user_id.email,
+        subject,
+        body
+      })
+
+      return {message: "Reminder sent."}
+        
+      })
 
     })
   }
