22e9fb4a4bd35e0f99232761c3cea42604e71fdf
Lalit <lalitnandan19@gmail.com>
2026-01-12 14:50:04 +0530

    Email added

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,7 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
+import Connection from "../models/Connection.js";
+import sendEmail from "../configs/nodeMailer.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "incanvas-app" });
@@ -58,5 +60,38 @@ const syncUserDeletion = inngest.createFunction(
   }
 );
 
+// Inngest Function to send Remainder when a new connection request is added
+const sendConnectionRequestReminder = inngest.createFunction(
+  {id:"send-new-connection-request-remainder"},
+  {event:"app/connection-request"},
+  async ({event,step})=>{
+    const {connectionId}=event.data;
+
+    await step.run('send-connection-request-mail',async()=>{
+      const connection=await Connection.findById(connectionId).populate('from_user_id','to_user_id');
+      const subject=`New Connection Request`;
+      const body=`
+      <div style="font-family:Arial,sans-serif;padding:20px;>
+      <h2>Hi ${connection.to_user_id.full_name},</h2>
+      <p>You have a new connection request from ${connection.from_user_id.full_name}-@${connection.from_user_id.username}</p>
+      <p>Click <a heref="${process.env.FRONTEND_URL}/connections" style="color:#10b981;"> here </a> to accept or reject the request </p>
+      <br/>
+      <p>Thanks, <br/> Incanvas - Stay Connected</p>
+      </div>
+      `;
+
+      await sendEmail({
+        to : connection.to_user_id.email,
+        subject,
+        body
+      })
+
+      // Want to send remainder in 24 hours
+      // ...
+
+    })
+  }
+)
+
 // Create an empty array where we'll export future Inngest functions
-export const functions = [syncUserCreation, syncUserUpdation, syncUserDeletion];
+export const functions = [syncUserCreation, syncUserUpdation, syncUserDeletion,sendConnectionRequestReminder];
